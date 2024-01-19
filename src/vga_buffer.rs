d24c5174a47eb7d76ce2592bc89a721bfba8661a
Logan Dhillon <logandhi64@gmail.com>
2024-01-19 11:05:06 -0500

    Create VGA buffer and writer

---
+++ src/vga_buffer.rs
@@ -0,0 +1,101 @@
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_char: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    // column position
+    col_pos: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    // write byte
+    pub fn write(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(), 
+            byte => {
+                if self.col_pos >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.col_pos;
+                let color_code = self.color_code;
+                
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_char: byte,
+                    color_code,
+                };
+
+                self.col_pos += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {}
+
+    pub fn write_str(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // is printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write(byte),
+                // is not part of printable ASCII range
+                _ => self.write(0xfe),
+            }
+        }
+    }
+}
+
+pub fn tmp() {
+    let mut writer = Writer {
+        col_pos: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_str("Hello, BadOS!")
+}
