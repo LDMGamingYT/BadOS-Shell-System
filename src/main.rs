8e0f1933d82544b13673a6068f6eb9c16f604500
Logan Dhillon <logandhi64@gmail.com>
2024-01-20 15:17:58 -0500

    Move QEMU functions into module `qemu`

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+mod qemu;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -24,7 +25,7 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
-    exit_qemu(QemuExitCode::Success);
+    qemu::exit_qemu(qemu::ExitCode::Success);
 }
 
 #[no_mangle]
@@ -35,22 +36,6 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
 }
 
 
