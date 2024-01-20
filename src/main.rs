b8266cc9e8a72489f06af4d5e5cc3d2a0d8fc9a2
Logan Dhillon <logandhi64@gmail.com>
2024-01-20 15:34:04 -0500

    Rename serial print methods

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serialln!("[failed]\n");
-    serialln!("Error: {}\n", info);
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
     qemu::exit(qemu::ExitCode::Failed);
     loop {}
 }
@@ -41,15 +41,15 @@ where
     T: Fn(),
 {
     fn run(&self) {
-        serial!("{}...\t", core::any::type_name::<T>());
+        serial_print!("{}...\t", core::any::type_name::<T>());
         self();
-        serialln!("[ok]");
+        serial_println!("[ok]");
     }
 }
 
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Testable]) {
-    serialln!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test.run();
     }
--- src/serial.rs
+++ src/serial.rs
@@ -19,7 +19,7 @@ pub fn _print(args: ::core::fmt::Arguments) {
 
 /// Prints to the host through the serial interface.
 #[macro_export]
-macro_rules! serial {
+macro_rules! serial_print {
     ($($arg:tt)*) => {
         $crate::serial::_print(format_args!($($arg)*));
     };
@@ -27,9 +27,9 @@ macro_rules! serial {
 
 /// Prints to the host through the serial interface, appending a newline.
 #[macro_export]
-macro_rules! serialln {
-    () => ($crate::serial!("\n"));
-    ($fmt:expr) => ($crate::serial!(concat!($fmt, "\n")));
-    ($fmt:expr, $($arg:tt)*) => ($crate::serial!(
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
         concat!($fmt, "\n"), $($arg)*));
 }
