db95ffc5e46d385593b357fc614c48a3701fe9a2
Logan Dhillon <logandhi64@gmail.com>
2024-01-20 14:58:06 -0500

    Create print(ln) macros

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,9 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    write!(vga_buffer::WRITER.lock(), "Hello, BadOS!\nNew lines!").unwrap();
-    write!(vga_buffer::WRITER.lock(), "\n\nLorem ipsum dolor sit amet sunt labore laborum incididunt qui deserunt officia pariatur velit nisi occaecat quis esse nisi consectetur tempor ut fugiat ut veniam proident veniam minim pariatur non et incididunt ex velit minim ea ex mollit in fugiat pariatur cupidatat duis anim magna ex in exercitation eiusmod exercitation proident deserunt anim aliquip cillum").unwrap();
+    println!("Hello, BadOS!");
+    print!("New lines!");
+    print!("\n\nLorem ipsum dolor sit amet sunt labore laborum incididunt qui deserunt officia pariatur velit nisi occaecat quis esse nisi consectetur tempor ut fugiat ut veniam proident veniam minim pariatur non et incididunt ex velit minim ea ex mollit in fugiat pariatur cupidatat duis anim magna ex in exercitation eiusmod exercitation proident deserunt anim aliquip cillum");
 
     loop {}
 }
