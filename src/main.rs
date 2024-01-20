35c45b037cb7e0c00cd9e9bd1b3d1ca0faf53b27
Logan Dhillon <logandhi64@gmail.com>
2024-01-20 15:27:34 -0500

    Move panic messages to serial port

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,19 @@ mod qemu;
 mod serial;
 
 #[panic_handler]
+#[cfg(not(test))]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+// panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serialln!("[failed]\n");
+    serialln!("Error: {}\n", info);
+    qemu::exit_qemu(qemu::ExitCode::Failed);
     loop {}
 }
 
@@ -44,6 +55,6 @@ pub extern "C" fn _start() -> ! {
 #[test_case]
 fn trivial_assertion() {
     serial!("trivial assertion... ");
-    assert_eq!(1, 1);
+    assert_eq!(1, 2);
     serialln!("[ok]");
 }
