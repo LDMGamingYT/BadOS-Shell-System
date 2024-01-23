36cd13ec4b21673d223031c3e03b21a622341269
Logan Dhillon <logandhi64@gmail.com>
2024-01-23 14:06:37 -0500

    Create interrupt handler

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,16 @@
 
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
-
 #![reexport_test_harness_main = "test_main"]
+
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
 pub mod serial;
 pub mod qemu;
 pub mod vga_buffer;
+pub mod interrupts;
 
 pub trait Testable {
     fn run(&self) -> ();
