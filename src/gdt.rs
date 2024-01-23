2883cb56914695e7b4fa924c91150520ebdd5874
Logan Dhillon <logandhi64@gmail.com>
2024-01-23 18:58:57 -0500

    Use x86_64 segment selectors

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,14 +1,10 @@
-use x86_64::VirtAddr;
+use x86_64::{VirtAddr, structures::gdt::SegmentSelector};
 use x86_64::structures::tss::TaskStateSegment;
 use lazy_static::lazy_static;
 
 use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor};
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
-
-pub fn init() {
-    GDT.load();
-}
 
 lazy_static! {
     static ref TSS: TaskStateSegment = {
@@ -26,10 +22,26 @@ lazy_static! {
 }
 
 lazy_static! {
-    static ref GDT: GlobalDescriptorTable = {
+    static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-        gdt.add_entry(Descriptor::kernel_code_segment());
-        gdt.add_entry(Descriptor::tss_segment(&TSS));
-        gdt
+        let code_sel = gdt.add_entry(Descriptor::kernel_code_segment());
+        let tss_sel = gdt.add_entry(Descriptor::tss_segment(&TSS));
+        (gdt, Selectors { code_sel, tss_sel })
     };
+}
+
+pub fn init() {
+    use x86_64::instructions::tables::load_tss;
+    use x86_64::instructions::segmentation::{CS, Segment};
+
+    GDT.0.load();
+    unsafe {
+        CS::set_reg(GDT.1.code_sel);
+        load_tss(GDT.1.tss_sel);
+    }
+}
+
+struct Selectors {
+    code_sel: SegmentSelector,
+    tss_sel: SegmentSelector,
 }
